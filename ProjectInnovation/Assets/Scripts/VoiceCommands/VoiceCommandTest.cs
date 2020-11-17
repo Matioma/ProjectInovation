c5110e516d670bc576e8fc7266c3077c46b1b2ce
Ana-Maria Ilea <62722821+Ana-MariaIlea@users.noreply.github.com>
2020-11-17 10:10:39 +0100

    Voice Commands Start

---
+++ ProjectInnovation/Assets/Scripts/VoiceCommands/VoiceCommandTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class VoiceCommandTest : MonoBehaviour
+{
+    private KeywordRecognizer keywordRecognizer;
+    private Dictionary<string, Action> actions = new Dictionary<string, Action>();
+
+
+    private void Start()
+    {
+        actions.Add("can you help me", Help);
+        actions.Add("give directions", Directions);
+        actions.Add("main menu", MainMenu);
+        actions.Add("quit", Quit);
+        actions.Add("are you here", AreYouHere);
+
+        keywordRecognizer = new KeywordRecognizer(actions.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += RecognizedSpeech;
+        keywordRecognizer.Start();
+
+
+
+    }
+
+    private void RecognizedSpeech(PhraseRecognizedEventArgs speeech)
+    {
+        Debug.Log(speeech.text);
+       // if (actions.ContainsKey(speeech.text))
+        //{
+            actions[speeech.text].Invoke();
+       // }
+    }
+
+    private void Help()
+    {
+        Debug.Log("Help on the way");
+    }
+    private void MainMenu()
+    {
+        Debug.Log("Main menu is open");
+    }
+    private void Directions()
+    {
+        Debug.Log("Here are the directions");
+    }
+
+    private void Quit()
+    {
+        Debug.Log("Are you sure you want to quit?");
+    }
+
+    private void AreYouHere()
+    {
+        Debug.Log("I am here");
+    }
+}
