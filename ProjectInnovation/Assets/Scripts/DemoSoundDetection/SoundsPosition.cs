c1b226a76de8d10beff9e8df76d99143493fce4f
Matioma <Artiom.Matievschi@gmail.com>
2020-11-20 11:57:50 +0100

    Add resonance Audio

---
+++ ProjectInnovation/Assets/Scripts/DemoSoundDetection/SoundsPosition.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundsPosition : MonoBehaviour
+{
+    [SerializeField]
+    List<Vector3> positions;
+    int currentSound = 0;
+
+    private void Start()
+    {
+        UpdateSoundPosition();
+    }
+
+    public void UpdateSoundPosition() {
+        if (currentSound >= positions.Count) {
+            #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+            #endif
+            // Application.Quit();
+            return;
+        }
+
+
+        Vector3 position = positions[currentSound];
+        transform.position = position;
+        currentSound++;
+    }
+
+}
---
+++ ProjectInnovation/Assets/Scripts/DemoSoundDetection/TargetSound.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetSound : MonoBehaviour
+{
+    [SerializeField]
+    GameObject soundTarget;
+
+    public float angleAccesptance = 50.0f;
+
+    int wins=0;
+    int loses=0;
+
+
+
+
+    private void OnApplicationQuit()
+    {
+        Debug.Log(wins + " / " + loses + " = " + ((float)wins)/((float)loses+wins)*100 + "%");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            float angle =GetAngle();
+            if (angle < angleAccesptance) {
+                wins++;
+            }
+            else {
+                loses++;
+            }
+            soundTarget.GetComponent<SoundsPosition>().UpdateSoundPosition();
+        }
+    }
+
+
+    float GetAngle() {
+        Vector3 difference = soundTarget.transform.position - this.transform.position;
+        float angles=0.0f;
+
+
+        float length1 = transform.forward.magnitude;
+        float length2 = difference.magnitude;
+
+
+        float dotProduct = Vector3.Dot(transform.forward, difference);
+
+
+        //if (dotProduct < -1)
+        //{
+        //    Debug.Log("look to opposite direction");
+        //    angles = -1w
+        //}
+        //else {
+            float anglesRadians = Mathf.Acos(dotProduct / (length1 * length2));
+            angles = anglesRadians *  Mathf.Rad2Deg;
+
+            Debug.Log(angles);
+        //}
+
+        return angles;
+    }
+
+}
