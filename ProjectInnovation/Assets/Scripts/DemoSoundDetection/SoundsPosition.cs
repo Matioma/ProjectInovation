27195d9ec61b070fbb04b97efa4d493c26e2ba29
Matioma <Artiom.Matievschi@gmail.com>
2020-11-23 10:02:31 +0100

    Audio Direction Test

--- ProjectInnovation/Assets/Scripts/DemoSoundDetection/SoundsPosition.cs
+++ ProjectInnovation/Assets/Scripts/DemoSoundDetection/SoundsPosition.cs
@@ -5,12 +5,30 @@ using UnityEngine;
 public class SoundsPosition : MonoBehaviour
 {
     [SerializeField]
+    int numberOfTests = 10;
+
+
+    [SerializeField]
     List<Vector3> positions;
     int currentSound = 0;
 
+
+    [SerializeField]
+    public Vector3 size;
+
     private void Start()
     {
+        for (int i = 0; i < numberOfTests; i++)
+        {
+            float positionX = Random.Range(0, size.x) - (size.x / 2);
+            float positionY = Random.Range(0, size.y) - (size.y / 2);
+            float positionZ = Random.Range(0, size.z) - (size.z / 2);
+            Vector3 RelativePositionRange = new Vector3(positionX, positionZ, positionY);
+
+            positions.Add(RelativePositionRange + transform.position);
+        }
         UpdateSoundPosition();
+       
     }
 
     public void UpdateSoundPosition() {
@@ -18,14 +36,18 @@ public class SoundsPosition : MonoBehaviour
             #if UNITY_EDITOR
             UnityEditor.EditorApplication.isPlaying = false;
             #endif
-            // Application.Quit();
             return;
         }
-
-
         Vector3 position = positions[currentSound];
         transform.position = position;
         currentSound++;
+    }
+
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(1, 0, 0);
+        Gizmos.DrawCube(transform.position, size);
     }
 
 }
---
+++ ProjectInnovation/Assets/Scripts/Editor/SoundRandom.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(SoundsPosition))]
+public class SoundRandom : Editor
+{
+    public void OnSceneGUI()
+    {
+
+        var t = (target as SoundsPosition);
+
+        //DrawGizmo.
+
+        //Gizmos.DrawCube(t.transform.position, t.start);
+
+
+        //EditorGUI.BeginChangeCheck();
+        //Vector3 pos = Handles.PositionHandle(t.lookAtPoint, Quaternion.identity);
+        //if (EditorGUI.EndChangeCheck())
+        //{
+        //    Undo.RecordObject(target, "Move point");
+        //    t.lookAtPoint = pos;
+        //    t.Update();
+        //}
+    }
+
+}
