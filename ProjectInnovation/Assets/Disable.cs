bb5de099dbf5b2f7025e7bb2ecd051f8e6908fb1
Matioma <Artiom.Matievschi@gmail.com>
2020-11-23 10:12:30 +0100

    TestSoundDirection

---
+++ ProjectInnovation/Assets/Disable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disable : MonoBehaviour
+{
+    [SerializeField]
+    float time;
+
+    private void Start()
+    {
+        Invoke("turnOff", time);
+    }
+
+
+    private void turnOff()
+    {
+        gameObject.SetActive(false);
+    }
+}
