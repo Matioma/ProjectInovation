2ba521a117e9575e4c87546b189e25bdc3b839fe
Matioma <Artiom.Matievschi@gmail.com>
2020-11-25 10:39:26 +0100

    TriggerSound On enther triggerZone

---
+++ ProjectInnovation/Assets/Scripts/TriggerSound.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerSound : MonoBehaviour
+{
+    [SerializeField]
+    AudioSource[] audioSouces;
+
+    int currentSound = 0;
+
+
+    void PlayNextSound() { 
+        if(audioSouces.Length <= 0)
+        {
+            return;
+        }
+        if (currentSound >= audioSouces.Length) {
+            return;
+        }
+
+        if (audioSouces[currentSound].isPlaying)
+        {
+            return;
+        }
+        else {
+            currentSound++;
+            audioSouces[currentSound].Play();
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayNextSound();
+        Debug.Log("Trigger!!");
+    }
+}
