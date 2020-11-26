37b8c28e07e7e79ef301c667064d92b73d3d1df3
Matioma <Artiom.Matievschi@gmail.com>
2020-11-26 14:48:24 +0100

    Sequence

--- ProjectInnovation/Assets/Scripts/Audio/AudioSequence.cs
+++ ProjectInnovation/Assets/Scripts/Audio/AudioSequence.cs
@@ -11,9 +11,9 @@ public class AudioSequence : MonoBehaviour
     bool startOnAwake=false;
 
 
+    [Tooltip("0-> start, 1 -> loop, 2- end sound")]
     [SerializeField]
     List<AudioClip> audioClips;
-
 
 
     int audioIndex =0;
@@ -47,19 +47,38 @@ public class AudioSequence : MonoBehaviour
             return;
         }
         sequnceStarted = true;
+        onSequenceStart?.Invoke();
         GetComponent<AudioSource>().PlayOneShot(audioClips[audioIndex]);       
     }
+
+    public void StopSequence() {
+        audio.Stop();
+
+        audio.clip = audioClips[2];
+        audio.loop = false;
+
+        audio.Play();
+        Debug.Log(audio.isPlaying);
+    }
+
 
     private void PlayNextAudio() {
         if (audioIndex < audioClips.Count - 1)
         {
             audioIndex++;
-            audio.PlayOneShot(audioClips[audioIndex]);
+            if (audioIndex == 1)
+            {
+                audio.clip = audioClips[audioIndex];
+                audio.loop = true;
+                audio.Play();
+            }
         }
         else {
             sequenceEnded = true;
+            onSequenceEnd?.Invoke();
         }
     }
+
 
     private void Update()
     {
